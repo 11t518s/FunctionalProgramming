a0f93ce1ccd28b373392a553e15a20dc4752e18d
ryuna <anottrx@gmail.com>
2022-12-18 12:16:18 +0900

    anottrx: 2주차 과제

--- src/2week/app.js
+++ src/2week/app.js
@@ -1,68 +1,79 @@
-var shopping_cart = [];
-var shopping_cart_total = 0;
+// 데이터
+let shoppingCart = [];
+const TAX_RATE = 0.1;
+const setShoppingCart = newShoppingCart => {
+  shoppingCart = newShoppingCart;
+};
 
+// 금액(1,000원)을 숫자로
+const convertPriceToNumber = price => {
+  return Number(price.replaceAll(/,|원/g, ''));
+};
+
+// 숫자 금액(1000)을 문구로(1,000원)
+const convertPriceToWord = price => {
+  // 금액이 자꾸 부족해서 우선 10을 임시로 곱했습니다 (1000원인데 100원이 뜨는 문제)
+  return `${(price * 10).toLocaleString()}원`;
+};
+
+// 클릭 액션
 document.querySelectorAll('button').forEach(button =>
   button.addEventListener('click', ({ target }) => {
     const name = target.parentNode.querySelector('.menu-name').textContent;
     const category = target.parentNode.querySelector('.category').textContent;
     const price = target.parentNode.querySelector('.price').textContent;
+    const priceNumber = convertPriceToNumber(price);
 
-    add_item_to_cart({ name, category, price });
+    const newCart = addItemToCart({ name, category, price: priceNumber });
+    setShoppingCart(newCart);
+    const cartTotalPrice = calcCartTotal(newCart);
+
+    handleDomUpdate(newCart, cartTotalPrice);
   }),
 );
 
-function add_item_to_cart(item) {
-  shopping_cart.push(item);
-  console.log(shopping_cart);
-  calc_cart_total();
-}
+const addItemOnArray = (array, item) => [...array, item];
+const getTotalFromArray = (array, element) => array.reduce((acc, cur) => acc + cur[`${element}`], 0);
+const updateTax = price => price * TAX_RATE;
+const updateTaxDom = shoppingCartTotal => setTaxDom(updateTax(shoppingCartTotal));
+const addItemToCart = cartItem => addItemOnArray(shoppingCart, cartItem);
+const calcCartTotal = shoppingCart => getTotalFromArray(shoppingCart, 'price');
 
-function calc_cart_total() {
-  shopping_cart_total = 0;
-  for (var i = 0; i < shopping_cart.length; i++) {
-    var item = shopping_cart[i];
-    shopping_cart_total += item.price;
-  }
-  set_cart_total_dom();
-  update_shipping_icons();
-  update_tax_dom();
-}
-
-function set_cart_total_dom() {
-  document.querySelector('.total-price').textContent = shopping_cart_total;
-}
-
-function update_shipping_icons() {
-  var buy_buttons = get_buy_buttons_dom();
-  for (var i = 0; i < buy_buttons.length; i++) {
-    var item = buy_buttons[i];
-    console.log(item);
-    if (item.price + shopping_cart_total >= 20) item.show_free_shopping_icon();
-    else item.hide_free_shopping_icon();
-  }
-}
-
-function get_buy_buttons_dom() {
-  var buttons = [];
-
-  for (var i = 0; i < shopping_cart.length; i++) {
-    var item = shopping_cart[i];
-    item.show_free_shopping_icon = function () {
+const addShowOrHideShoppingIconOnArray = array =>
+  array.map(item => ({
+    ...item,
+    showFreeShoppingIcon: () => {
       console.log('DOM 의 아이콘을 보여줍니다');
-    };
-    item.hide_free_shopping_icon = function () {
+    },
+    hideFreeShoppingIcon: () => {
       console.log('DOM 의 아이콘을 숨깁니다');
-    };
-    buttons.push(item);
-  }
+    },
+  }));
 
-  return buttons;
-}
+const handleDomUpdate = (shoppingCart, shoppingCartPriceTotal) => {
+  setCartTotalDom(shoppingCartPriceTotal);
+  const buyButtons = addShowOrHideShoppingIconOnArray(shoppingCart);
 
-function update_tax_dom() {
-  set_tax_dom(shopping_cart_total * 0.1);
-}
+  updateShippingIcons(buyButtons, shoppingCartPriceTotal);
+  updateTaxDom(shoppingCartPriceTotal);
+};
 
-function set_tax_dom(value) {
-  document.querySelector('.total-price').textContent = value;
-}
+const updateShippingIcons = (buyButtons, shoppingCartPriceTotal) => {
+  buyButtons.forEach(buyButton => {
+    if (Number(buyButton.price + shoppingCartPriceTotal) >= 20000) {
+      console.log(buyButton.price, shoppingCartPriceTotal);
+      buyButton.showFreeShoppingIcon();
+    } else {
+      buyButton.hideFreeShoppingIcon();
+    }
+  });
+};
+
+// dom을 변경하는 로직
+const setTaxDom = tax => {
+  const cartTotalPriceWord = convertPriceToWord(tax);
+  document.querySelector('.total-price').textContent = cartTotalPriceWord;
+};
+const setCartTotalDom = shoppingCartTotal => {
+  document.querySelector('.total-price').textContent = shoppingCartTotal;
+};
